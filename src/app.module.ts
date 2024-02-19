7453f00ad99081e540561f1e9dce7c9202ff569b
Bertin M <bertin.m2001@gmail.com>
2024-02-19 09:56:24 +0200

    added resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogsModule } from './blogs/blogs.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    BlogsModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: '',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
